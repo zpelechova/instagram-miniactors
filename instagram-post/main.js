d034050de53899bab441abbb780679b6583d7156
Zuzana Pelechová <zpelechova@gmail.com>
2022-04-04 17:41:33 +0200

    dedupe of inputs

--- instagram-post/main.js
+++ instagram-post/main.js
@@ -12,26 +12,18 @@ Apify.main(async () => {
         console.log("What are you trying to get? It seems you forgot to add any input.")
     }
 
-    console.log("1 - " + usernames);
-
     usernames = Array.from(new Set(usernames))
-
-    console.log("2 - " + usernames);
     
     let directUrls = [];
     for (const u in usernames) {
         if (usernames[u].toLowerCase().includes('instagram.com/')) {
             directUrls.push(usernames[u]) 
-            console.log("3 - " + usernames[u]);
        
         } else {
             const directUrl = `https://www.instagram.com/${usernames[u]}`;
             directUrls.push(directUrl);
-            console.log("4 - " + directUrl);
         }
     }
-
-    console.log("5 - " + directUrls);
     
 
     const extendOutputFunction = async ({ data, item, itemSpec, page, request, customData, label }) => {
